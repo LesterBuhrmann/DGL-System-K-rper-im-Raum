5225dc327ce751d9431fb9fa773cc42b02ceaa1c
Lestosterone <51245494+Lestosterone@users.noreply.github.com>
2019-06-13 12:15:17 +0200

    Einführung des Hauptprogramms
    Die für die Bewegung relevanten Vektoren werden als Array über "vector" deklariert.
    Das Eulerverfahren zum numerischen Lösen von Differentialgleichungen wird für Arrays der für die Bewegung relevanten Vektoren in der for-Schleife angewandt. Die gewünschten Vektoren werden für jeden Zeitschritt ausgegeben.

--- main.cc
+++ main.cc
@@ -39,6 +39,32 @@ std::ostream &operator<<(std::ostream &Str, Vector const &v)
 
 int main()
 {
+  vector<Vector> xe, xm, ve, vm, ae, am;
+Vector xe0(0, 0, 0), xm0(1e6, 1e6, 1e6), ve0(0, 0, 0), vm0(0, 0, 0);
+
+xe.push_back(xe0);
+xm.push_back(xm0);
+ve.push_back(ve0);
+vm.push_back(vm0);
+
+vector<double> dt;
+dt.push_back(0.01)    
+
+ for(int i = 0;i <= 10000; ++i){
+Vector r = xm[i] + (xe[i]*m);
+ae.push_back(r*(mm/((r*r)*norm(r)))*G);
+am.push_back((ae[i]*m)*(me/mm));
+ve.push_back(ve[i]+(ae[i]*dt[i]));
+vm.push_back(vm[i]+(am[i]*dt[i]));
+xe.push_back(xe[i]+ve[i]*dt[i]);
+xm.push_back(xm[i]+vm[i]*dt[i]);
+dt.push_back(dt[i]);
+
+cout << norm(vm[i]) << " " << norm(r) << endl;
+
+
+
+ } 
     return 0;
 }
 
