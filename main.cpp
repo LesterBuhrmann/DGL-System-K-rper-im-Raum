5fe3306fa59a8486b082b9800360e3a63f625d0a
Lestosterone <51245494+Lestosterone@users.noreply.github.com>
2019-06-25 19:29:21 +0200

    Gleichmäßige Zeitachse für plot der Textdateien
    Ein Problem für das Erzeugen der Textdateien ist die Ungleichmäßigkeit des Zeitschritts. Durch die hier durchgeführten Änderungen ist es möglich, eine Text Datei mit Werten, die von Zeile zu Zeile den gleichen Zeitabstand haben, zu erstellen.
    Dafür wird eine Funktion zur Rundung von Zahlen des Typs double (Im Internet gefunden) verwendet.

--- main.cpp
+++ main.cpp
@@ -40,6 +40,13 @@ std::ostream &operator<<(std::ostream &Str, Vektor const &v) //Ein Operator, um 
 }
 
 
+double runden(double x, int genauigkeit) {         //Funktion für das Runden von Zahlen
+double Wert = pow(10.0, genauigkeit);
+x *= Wert;
+x += 0.5;
+x /= Wert;
+return x;  }
+
 int main()
 {
  double me = 5.96e24;  //Masse der Erde
@@ -57,19 +64,33 @@ xm.push_back(xm0);
 ve.push_back(ve0);
 vm.push_back(vm0);
 
-vector<double> dt;            //Die Länge des Zeitschritts dt ist vom Abstand und der Geschwindigkeit abhängig, weshalb dafür ein Array angelegt wird.
-dt.push_back(0.1);
+vector<double> dt,t,k;   //Die Länge des Zeitschritts dt ist vom Abstand und der Geschwindigkeit abhängig, weshalb dafür ein Array angelegt wird.
+                         //t ist der absolute Zeitwert und k wird benötigt um gerundete Zeitpunkte zu bilden 
+t.push_back(0);     //Startzeitpunkt für t
+dt.push_back(0.1); //Anfangszeitschritt nicht unnötig fein gewählt
+
+vector<int> tn; // Dies werden die Zeitschritte für die als txt Datei ausgegebenen Werte mit gleichmäßigem zeitlichen Abstand werden  
+  
+  
 int N = 20000;
  for(int i = 0;i <= N; ++i){     //Nach dem Euler-Verfahren wird hier die Bewegung der Körper schrittweise numerisch ermittelt.
 Vektor r = xm[i] + (xe[i]*m);        //Abstandsvektor
 ae.push_back(r*(mm/((r*r)*norm(r)))*G); //Beschleunigungsvektor der Erde nach newtonscher Gravitation
 am.push_back((ae[i]*m)*(me/mm));     //Beschhleunigungsvektor des Mars
-ve.push_back(ve[i]+(ae[i]*dt[i]));   //Geschwindigkeitsvektor der Erde über Tangentenzerlegung von ve
+ve.push_back(ve[i]+(ae[i]*dt[i]));   //Geschwindigkeitsvektor der Erde über Tangentenzerlegung von ve des vorherigen Zeitpunkts
 vm.push_back(vm[i]+(am[i]*dt[i]));   //Geschwindihkeitsvektor des Mars
-xe.push_back(xe[i]+ve[i]*dt[i]);     //Ortsvektor der Erde über Tangentenzerlegung von xe
+xe.push_back(xe[i]+ve[i]*dt[i]);     //Ortsvektor der Erde über Tangentenzerlegung von xe des vorherigen Zeitpunkts
 xm.push_back(xm[i]+vm[i]*dt[i]);     //Ortsvekor des Mars
 
 
+if(t[i] >= runden(t[i], 1)) {   //Wenn der Momentane Zeitwert genau um 0.1 größer als der auf eine Stelle gerundete Zeitwert ist,
+k.push_back(runden(t[i], 1));   //wird in k der momentane gerundete Zeitwert eingeschoben
+}                               //Damit erhält man ein Array k, dass genau um 0.1 weiterspringt, wenn t gerade knapp größer als k+0.1 ist.
+else{
+k.push_back(k[i-1]);  //Sonst wird der vorherige Wert eingeschoben
+}
+   
+   
 cout << xm[i] << " " << xe[i] << " " << norm(r) << endl;      //Ausgabe beliebiger Größen
 
 if((0.1 >= ((norm(r)*norm(r))/norm(vm[i]))*1e-8) && (norm(vm[i])/(norm(r)*norm(r)) >= 1e-8))    //Kontrolle des v zu r^2 Verhältnisses
@@ -82,14 +103,22 @@ if((0.1 >= ((norm(r)*norm(r))/norm(vm[i]))*1e-8) && (norm(vm[i])/(norm(r)*norm(r
                           }
 
  }
-
- FILE* fp;
+  
+ for(int i = 1; i <= N; i++){  //Hiermit wird tn so befüllt,     
+ if(k[i] > k[i-1]){            //dass es immer die Nummern der Einträge zeigt, die zeitlich fast genau 0.1 Einheiten entfernt sind.  
+ tn.push_back(i);}
+                   }
+ int Schrittzahl = tn.size();  //Anzahl der Schritte mit gleichen Zeitabständen 
+                               //Damit werden genau so viele Elemente wie möglich, aber nicht mehr in die Textdatei geschrieben 
+  
+  
+  FILE* fp;
  fp = fopen("xMars.txt","w");
- for(int j = 0;j <= N; j++)
+ for(int j = 0;j <= Schrittzahl; j++)
  {
 
 
-fprintf(fp,"%e %e %e\n", xm[j].x, xm[j].y, xm[j].z);
+fprintf(fp,"%e %e %e\n", xm[tn[j]].x, xm[tn[j]].y, xm[tn[j]].z);
 
  }
 fclose(fp);
@@ -97,11 +126,11 @@ fclose(fp);
 
 FILE* fs;
  fs = fopen("xErde.txt","w");
- for(int j = 0;j <= N; j++)
+ for(int j = 0;j <= Schrittzahl; j++)
  {
 
 
-fprintf(fs,"%e %e %e\n", xe[j].x, xe[j].y, xe[j].z);
+fprintf(fs,"%e %e %e\n", xe[tn[j]].x, xe[[j]].y, xe[[j]].z);
 
  }
 fclose(fs);
