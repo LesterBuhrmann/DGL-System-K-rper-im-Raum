801d49cdbe9dcedfb52f7935a5bb691cac3fcb0f
Lestosterone <51245494+Lestosterone@users.noreply.github.com>
2019-07-08 10:35:01 +0200

    Gleiche Dateien included wie Header

--- Vektor.cpp
+++ Vektor.cpp
@@ -2,26 +2,31 @@
 #include<iostream>
 #include<vector>
 #include<math.h>
+#include<string>
+#include<algorithm>
+#include<fstream>
+
+
 
 
 using namespace std;
 
 Vektor::Vektor(double x, double y, double z) : x(x), y(y), z(z) {}
-  Vektor Vektor::operator+(const Vektor &b)                        //Einführung des Operators für Vektoraddition
+  Vektor Vektor::operator+(const Vektor &b)                        //EinfÃ¼hrung des Operators fÃ¼r Vektoraddition
   {
     Vektor summe(x +b.x, y + b.y, z +b.z);
     return summe;
   }
-double Vektor::operator*(const Vektor &a)                 //Einführung des Operators für Skalarprodukt von Vektoren
+double Vektor::operator*(const Vektor &a)                 //EinfÃ¼hrung des Operators fÃ¼r Skalarprodukt von Vektoren
 {
 double produkt(x*a.x+y*a.y+z*a.z);
 return produkt;
 }
-Vektor Vektor::operator*(const double &a)                 //Einführung des Operators für rechtsseitige Multiplikation mit einem Skalar
+Vektor Vektor::operator*(const double &a)                 //EinfÃ¼hrung des Operators fÃ¼r rechtsseitige Multiplikation mit einem Skalar
 {
     Vektor pro(a*x, a*y, a*z);
     return pro;
 }
-double norm(Vektor a) {                          //Einführung der Betragsfunktion
+double norm(Vektor a) {                          //EinfÃ¼hrung der Betragsfunktion
     return sqrt(a * a);
 }
